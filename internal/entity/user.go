2ee1428f370ecccf8976a8a8cf1c1e6d8c0fc720
samarskiyad <samarskiyad@student.bmstu.ru>
2025-07-21 20:06:22 +0300

    Update validation parameters

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -4,11 +4,11 @@ import "time"
 
 type User struct {
 	ID           int       `db:"id"`
-	Login        string    `db:"login"`
-	Name         string    `db:"first_name"`
-	Surname      string    `db:"last_name"`
-	PasswordHash []byte    `db:"-"`
-	PasswordSalt []byte    `db:"-"`
-	CreatedAt    time.Time `db:"created_at"`
-	UpdatedAt    time.Time `db:"updated_at"`
+	Login        string    `db:"login" valid:"required,alphanum,length(3|50)"`
+	Name         string    `db:"first_name" valid:"required,utfletter,length(1|100)"`
+	Surname      string    `db:"last_name" valid:"required,utfletter,length(1|100)"`
+	PasswordHash []byte    `db:"-" valid:"-"`
+	PasswordSalt []byte    `db:"-" valid:"-"`
+	CreatedAt    time.Time `db:"created_at" valid:"-"`
+	UpdatedAt    time.Time `db:"updated_at" valid:"-"`
 }
